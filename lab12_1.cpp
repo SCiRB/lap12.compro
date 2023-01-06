7067ace6ea45b588020560a23e763206086a2a62
Lica <Tanapatlica@gmail.com>
2023-01-06 16:51:36 +0700

    lap 12 completed

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,2 +1,31 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<ctime>
+#include<cstdlib>
+
+using namespace std;
+
+int main(){
+    cout << "Press Enter 3 times to reveal your future.";
+    char grade;
+    int i;
+    srand(time(0));
+    i = rand()%9+1;
+    if(i==1){grade = 'A';}
+    if(i==2){grade = 'B';}
+    if(i==3){grade = 'B';}
+    if(i==4){grade = 'C';}
+    if(i==5){grade = 'C';}
+    if(i==6){grade = 'D';}
+    if(i==7){grade = 'D';}
+    if(i==8){grade = 'F';}
+    if(i==9){grade = 'W';}
+    cin.get();
+    cin.get();
+    cin.get();
+    cout << "You will get " << grade;
+    if (i == 2||i == 4||i == 6){
+        cout << "+";
+    }
+    cout << " in this 261102.";
+    return 0;
+}
